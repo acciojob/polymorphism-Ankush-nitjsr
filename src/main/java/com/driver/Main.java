010a31d96911d87651dfc5da47239963f621a444
Ankush <ankushnitjsr@gmail.com>
2023-04-13 17:55:44 +0530

    Modify class main

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,27 +3,20 @@ package com.driver;
 public class Main {
     public static class Product{
         public int product(int x, int y){
-            return x+y;
+             return x+y;
         }
         public int product(int x, int y, int z){
             return x+y+z;
         }
         public double product(double x, double y){
-            return x*y;
+            return x+y;
         }
     }
     public static void main(String[] args) {
-
         Product p = new Product();
-        int output;
-        output = p.product(1, 2);
-        //System.out.println(output);
-        int output2;
-        output2 = p.product(1, 2, 3);
-        //System.out.println(output2);
-        double output3;
-        output3 = p.product(2.5, 3.5);
-        //System.out.println(output3);
+        p.product(1, 2);
+        p.product(1, 2, 3);
+        p.product(2.5, 3.5);
     }
 }
 
